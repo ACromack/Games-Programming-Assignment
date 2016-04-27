b6c89d390c2643553e3713b044ecbc771ca1ac47
Ashley Cromack <Cromazone@live.co.uk>
2016-04-27 16:49:38 +0100

    Loading
    Added loading class

---
+++ sdl2template-master/src/loading.cpp
@@ -0,0 +1,18 @@
+// C++ File for the loading screen class
+#include "loading.h"
+#include <iostream>
+
+loadingClass::loadingClass()
+{
+	std::cout << "Loading Screen Constructor!  " << std::endl;
+}
+
+void loadingClass::loadScreen()
+{
+
+}
+
+loadingClass::~loadingClass()
+{
+	std::cout << "Loading Screen Destructor!  " << std::endl;
+}
---
+++ sdl2template-master/src/loading.h
@@ -0,0 +1,20 @@
+// Header file for the loading screen class
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+
+class loadingClass
+{
+	private:
+
+
+
+	public:
+		loadingClass();
+
+		void loadScreen();
+
+		~loadingClass();
+};
